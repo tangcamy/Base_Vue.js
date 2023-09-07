3cc91d2be48f864f60e6ab250804165409e48f69
tangcamy <camytang12@gmail.com>
2023-09-07 11:59:40 +0800

    info.vue options to compostion

--- src/components/headers/info.vue
+++ src/components/headers/info.vue
@@ -1,35 +1,70 @@
-<script>
-export default {
-  // 元件(接收上層給予的數值，嚴謹一點規定變數屬性 (required是否必填;dafault預設值))
-  props:{
-    devices:{
-      type:Array,
-      required:true,
-      default:[],
-    }
-  },
-  data(){
-    return {
-      aa:0,
-    }
-  },
+<script setup>
+import { computed } from 'vue';
+import {defineProps} from 'vue'; //必須於< script setup>時使用，不然會出錯
 
-  // 呼叫的時候不需要deviceCount()，method才需要
-  computed:{
-    deviceCount(){
-      return this.devices.length
-    },
-    noDevice(){
-      return this.deviceCount === 0
-    }
+// 取得App.vue中的devices數量
+const props = defineProps({
+  devices:{
+  type: Array,
+  required: true,
+  default: [],
   }
+})
+
+// 需要將defineProps 用一個props變數存取，才能透過props.devices取得資料
+// 如果直接definProps , 下面devices.length會出錯
+
+const deviceCount = computed(()=>props.devices.length) //簡化函式寫法，把大括號＆return拿掉即可
+// const deviceCount = computed(()=>{
+//   return props.devices.length
+// })
+
+const noDevice = computed(() => deviceCount.value === 0)//簡化函式寫法，把大括號＆return拿掉即可
+// const noDevice = computed(()=>{
+//   // deviceCount是一個ref，取值要用.value
+//   return deviceCount.value === 0
+// })
+
+
+// export default {
+//   setup(){
+//     const props = defineProps({
+//       devices:{
+//         type: Array,
+//         required: true,
+//         default: [],
+//       }
+//     })
+
+//     return props
+
+//   },
+
   
-}
-</script>
+  // - 元件(接收上層給予的數值，嚴謹一點規定變數屬性 (required是否必填;dafault預設值))  ; (optition-->compostion 放置setup)
+  // props:{
+  //   devices:{
+  //     type:Array,
+  //     required:true,
+  //     default:[],
+  //   }
+  // },
+
+  // // - computed 呼叫的時候不需要deviceCount()，method才需要; (optition-->compostion 放置setup)
+  // computed:{
+  //   deviceCount(){
+  //     return this.devices.length
+  //   },
+  //   noDevice(){
+  //     return this.deviceCount === 0
+  //   }
+  // }
+  
+// }
+// </script>
 
 
 <template>
-
   <h3 v-if="noDevice">目前無任何設備</h3> 
   <h3 v-else>目前共計 {{ deviceCount }} 項設備</h3>
 </template>
